d8066b69f0bc3c4698840329171a24f18906a537
quyennguyencoder <quyenn0301@ut.edu.vn>
2026-02-01 21:57:08 +0700

    first commit

---
+++ src/test/java/com/nguyenquyen/vnpaydemo/VnpayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenquyen.vnpaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnpayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
